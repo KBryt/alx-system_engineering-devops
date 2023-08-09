c870a51c97c07df588b252f8d69c37bb0e5e021f
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-08-08 22:53:28 -0400

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,7 @@
 number of subscribers for a given subreddit. """
 
 import requests
-headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/Certifieddonnie)"}
+headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/KBryt)"}
 
 
 def number_of_subscribers(subreddit):
